c16a9cdc759be956ac147f7411c219ac95d3087b
Fernando Garcia <fer_930401@hotmail.com>
2017-05-25 10:41:13 -0500

    Capa CLPortalUsersData
    se agrego la capa de CLPortalUsersData

---
+++ CLPortalUsersData/DALSecurity.vb
@@ -0,0 +1,27 @@
+﻿'--------------------------------------------------------------------------------------------------
+'Class Name     : DALSecurity
+'Description    : This module handles Data Access Logic of Procedures using LINQ for Security
+'--------------------------------------------------------------------------------------------------
+Imports CLPortalUsersDataLink
+Imports CLPortalAbstract
+
+Public Class DalSecurity
+    Private ReadOnly _obj As New Factura
+    Public Function GetPortalUser(ByVal pUser As String, ByVal pPassword As String) As PerfilesUser 'As System.Collections.Generic.List(Of datos_user)
+        Try
+            Dim pf As PerfilesUser = _obj.GetPortalUser(pUser, pPassword)
+            Return pf 'pf.perfil.ToList()
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+    Public Function GetConfig(ByVal noInf As Integer) As List(Of ConfigGral)
+        Dim confg As List(Of ConfigGral)
+        Try
+            confg = _obj.GetConfig(10)
+        Catch ex As Exception
+            Throw ex
+        End Try
+        Return confg
+    End Function
+End Class
---
+++ CLPortalUsersData/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CLPortalUsersData")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("CLPortalUsersData")> 
+<Assembly: AssemblyCopyright("Copyright ©  2009")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0c09c8da-a7de-43f3-98ad-940f07022edc")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
